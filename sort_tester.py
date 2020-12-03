1f92ce2a7bd9e3da25d69475907bd4606d7889f3
Ludvig Kindberg <chandazz96@gmail.com>
2020-12-03 22:52:24 +0100

    Add pass by value dress function

--- sort_tester.py
+++ sort_tester.py
@@ -1,11 +1,21 @@
 from selection_sort.selection_sort import selection_sort
 from insertion_sort.insertion_sort import insertion_sort
+import copy
 
+def pass_by_value(f):
+    def _f(*args, **kwargs):
+        args_copied = copy.deepcopy(args)
+        kwargs_copied = copy.deepcopy(kwargs)
+        return f(*args_copied, **kwargs_copied)
+    return _f
+
+@pass_by_value
 def test_selection_sort(test_list):
     print("Before sorting: \n {}".format(test_list))
     sorted = selection_sort(test_list)
     print("After sorting: \n {}".format(sorted))
 
+@pass_by_value
 def test_insertion_sort(test_list):
     print("Before sorting: \n {}".format(test_list))
     sorted = insertion_sort(test_list)
